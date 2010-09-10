60a381505708062a4566f96b3f039ccdde9ba44c
Lucas Florio <lucasefe@gmail.com>
2010-09-10 16:30:32 -0300

    New context

--- test/assets_controller_test.rb
+++ test/assets_controller_test.rb
@@ -2,13 +2,15 @@ require "test_helper"
 
 module ThemeSupport
   class AssetsControllerTest < ActionController::TestCase  
-    tests ApplicationController
-    should "respond_to theme" do
-      assert @controller.respond_to?(:theme)
-    end
-    should "should store the theme's name" do
-      @controller.theme :default
-      assert_equal @controller.theme_name, :default
+    context "as an instance" do
+      tests ApplicationController
+      should "respond_to theme" do
+        assert @controller.respond_to?(:theme)
+      end
+      should "should store the theme's name" do
+        @controller.theme :default
+        assert_equal @controller.theme_name, :default
+      end
     end
   end
 end
