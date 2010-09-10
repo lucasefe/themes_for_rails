6eb111fa4a0f38f38d9c3ff9412831a2a493d42f
Lucas Florio <lucasefe@gmail.com>
2010-09-10 10:07:37 -0300

    Basic spec/test infrastructure.

---
+++ spec/routes_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe ThemeSupport::Routes do
+  it "shoudl be true" do
+    true.should be_true
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
+ENV["RAILS_ENV"] = "test"
+$:.unshift File.dirname(__FILE__)
+
+require "dummy_app/config/environment"
 require 'rspec'
 require 'rspec/autorun'
-require 'active_support'
-require 'active_record'
-require 'action_controller'
-require 'action_view'
-require 'rails3_plugin_toolbox'
+require "#{File.dirname(__FILE__)}/../lib/theme_support.rb"
+
+$:.unshift File.expand_path('../support', __FILE__)
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+# require 'active_support'
+# require 'active_record'
+# require 'action_controller'
+# require 'action_view'
+# require 'rails3_plugin_toolbox'
 
