02ca4b0fe4d833d5c63eaa1d0ea2656c01441e55
Lucas Florio <lucasefe@gmail.com>
2010-09-10 16:27:49 -0300

    Adding controller#theme_name

--- lib/theme_support/controller_methods.rb
+++ lib/theme_support/controller_methods.rb
@@ -1,6 +1,9 @@
 module ThemeSupport
   module ControllerMethods
     extend ActiveSupport::Concern
+    included do
+      attr_accessor :theme_name
+    end
     module ClassMethods
       def theme(theme_name)
         puts "setting theme #{theme_name}!"
@@ -8,7 +11,7 @@ module ThemeSupport
     end
     module InstanceMethods
       def theme(theme_name)
-        puts "setting theme #{theme_name}!"
+        @theme_name = theme_name
       end
     end
   end
--- test/assets_controller_test.rb
+++ test/assets_controller_test.rb
@@ -3,10 +3,12 @@ require "test_helper"
 module ThemeSupport
   class AssetsControllerTest < ActionController::TestCase  
     tests ApplicationController
-    
     should "respond_to theme" do
       assert @controller.respond_to?(:theme)
     end
-    
+    should "should store the theme's name" do
+      @controller.theme :default
+      assert_equal @controller.theme_name, :default
+    end
   end
 end
