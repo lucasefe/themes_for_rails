2d09707371eacdb3769ba8dc3184e97edd7507ef
Lucas Florio <lucasefe@gmail.com>
2010-09-11 16:02:11 -0300

    It's not gonna work if I don't add the View Helpers file.

---
+++ lib/themes_for_rails/view_helpers.rb
@@ -0,0 +1,37 @@
+module ThemesForRails
+  module ViewHelpers
+    extend ActiveSupport::Concern
+    included do
+      include ThemesForRails::CommonMethods
+    end
+    module InstanceMethods
+      def current_theme_stylesheet_path(asset)
+        base_theme_stylesheet_path(:theme => self.theme_name, :asset => asset, :extension => 'css')
+      end
+      def current_theme_javascript_path(asset)
+        base_theme_javascript_path(:theme => self.theme_name, :asset => asset, :extension => 'js')
+      end
+      def current_theme_image_path(asset)
+        image, extension = asset.split(".")
+        self.base_theme_image_path(:theme => self.theme_name, :asset => image, :extension => extension)
+      end
+      alias_method :theme_image_path, :current_theme_image_path
+      alias_method :theme_javascript_path, :current_theme_javascript_path
+      alias_method :theme_stylesheet_path, :current_theme_stylesheet_path
+      
+      def theme_image_tag(source)
+        image_tag(theme_image_path(source))
+      end
+      def theme_javascript_include_tag(*files)
+        files.collect! {|file| theme_javascript_path(file) }
+        javascript_include_tag *files
+      end
+      def theme_stylesheet_link_tag(*files)
+        files.collect! {|file| theme_stylesheet_path(file) }
+        stylesheet_link_tag *files
+      end
+    end
+  end
+end
+
+ActiveSupport.on_load(:action_view) { include ThemesForRails::ViewHelpers }
