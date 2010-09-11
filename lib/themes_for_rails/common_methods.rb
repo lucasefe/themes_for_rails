15c70010394e5b266d3bf881957d4f111a141c15
Lucas Florio <lucasefe@gmail.com>
2010-09-11 12:53:00 -0300

    Some refactoring

--- lib/themes_for_rails/common_methods.rb
+++ lib/themes_for_rails/common_methods.rb
@@ -4,38 +4,45 @@ module ThemesForRails
       File.join(theme_path_for(theme), "views")
     end
     def theme_name
-      case @theme_name
-      when Symbol then self.send(@theme_name)
-      when String then @theme_name
-      else
-        nil
+      @cached_theme_name ||= begin
+        case @theme_name
+        when Symbol then self.send(@theme_name)
+        when String then @theme_name
+        else
+          nil
+        end
       end
     end
     def theme_name=(name)
       @theme_name = name
     end
     def set_theme(name)
-      self.theme_name = name
+      self.theme_name = name    
+      add_theme_view_path unless self.theme_name.nil?
+    end
+    # will add the view path for the current theme
+    def add_theme_view_path
       add_theme_view_path_for(self.theme_name)
     end
-    def add_theme_view_path_for(theme_name)
-      self.view_paths << ActionView::FileSystemResolver.new(view_path_for(theme_name))
+    # will add the view path for a given theme name
+    def add_theme_view_path_for(name)
+      self.view_paths << ActionView::FileSystemResolver.new(view_path_for(name))
     end
     
-    def theme_path_for(theme_name, base = ::Rails.root)
-      File.join(base, "themes", theme_name.to_s)
+    def theme_path_for(name, base = ::Rails.root)
+      File.join(base, "themes", name.to_s)
     end
     
-    def theme_stylesheet_path(theme, asset)
-      File.join(theme_path_for(theme), 'stylesheets', "#{asset}.css")
+    def theme_stylesheet_path(name, asset)
+      File.join(theme_path_for(name), 'stylesheets', "#{asset}.css")
     end
-    def theme_javascript_path(theme, asset)
-      File.join(theme_path_for(theme), 'javascripts', "#{asset}.js")
+    def theme_javascript_path(name, asset)
+      File.join(theme_path_for(name), 'javascripts', "#{asset}.js")
     end
-    def theme_image_path(theme, asset, extension = nil)
+    def theme_image_path(name, asset, extension = nil)
       extension ||= "png"
       extension = ".#{extension}"
-      File.join(theme_path_for(theme), 'images', "#{asset}#{extension}")
+      File.join(theme_path_for(name), 'images', "#{asset}#{extension}")
     end
     
   end
