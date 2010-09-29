d562398ce911be7540f473969b3ca839754d4c9a
Lucas Florio <lucasefe@gmail.com>
2010-09-29 14:27:23 -0300

    should infer automatically the themes dir.

--- test/themes_for_rails_test.rb
+++ test/themes_for_rails_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class ThemesForRailsTest < Test::Unit::TestCase
   should 'know the available themes' do
-    assert_equal ThemesForRails.available_theme_names, ['default']
+    assert_equal ['default'], ThemesForRails.available_theme_names
   end
 end
