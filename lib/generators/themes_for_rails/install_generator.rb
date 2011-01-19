18dca717ade3a7e0f98ff325bb932e04d83dd761
Lucas Florio <lucasefe@gmail.com>
2011-01-19 10:08:12 -0300

    Adding themes_for_rails automatically when calling the themes_for_rails:install generator. (matheusmoreira, thanks)

--- lib/generators/themes_for_rails/install_generator.rb
+++ lib/generators/themes_for_rails/install_generator.rb
@@ -6,6 +6,11 @@ module ThemesForRails
       def create_themes_folder
         empty_directory(File.join(Rails.root, 'themes'))
       end
+      
+      def add_themes_for_rails_routes
+        route "themes_for_rails"
+      end
+      
     end
   end
 end
