c31f66fa29586eb36df085f97670db1630fefbed
Lucas Florio <lucasefe@gmail.com>
2011-02-02 11:04:32 -0300

    Fixing some ugly looking tests. Thanks to jbarreneche

--- test/view_helpers_test.rb
+++ test/view_helpers_test.rb
@@ -17,11 +17,11 @@ module ThemesForRails
     end
 
     should 'delegate to stylesheet_link_tag' do
-      assert theme_stylesheet_link_tag('cuac.css').include?('media="screen"')
+      assert_match /media=.screen/, theme_stylesheet_link_tag('cuac.css')
     end
 
     should 'delegate options (lazy testing, I know)' do
-      assert theme_stylesheet_link_tag('cuac.css', :media => 'print').include?('media="print"')
+      assert_match /media=.print/, theme_stylesheet_link_tag('cuac.css', :media => 'print')
     end
     should 'delegate options in image_tag' do
       assert_match /width=.40/, theme_image_tag('image.css', :size => '40x50')
