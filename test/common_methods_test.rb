2011c8e428e61dd83374c02c1ba78e5fd9434c4e
Juan Manuel Barreneche <snipperme@gmail.com>
2011-02-02 10:21:20 -0300

    Fix common methods to build path from configuration data

---
+++ test/common_methods_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ThemesForRailsTest < Test::Unit::TestCase
+  setup do
+    @common = Object.new
+    @common.extend ThemesForRails::CommonMethods
+    @common.theme_name = "awesome"
+    ThemesForRails.config.clear
+  end
+  should 'use config base_dir to build theme path' do
+    ThemesForRails.config.base_dir ='some_path'
+    assert_match /some_path/, @common.theme_path
+  end
+  should 'use config themes_dir to build theme path' do
+    ThemesForRails.config.themes_dir ='skinner'
+    assert_match /skinner/, @common.theme_path
+  end
+  should 'use config base_dir to build theme path for theme' do
+    ThemesForRails.config.base_dir ='some_path'
+    assert_match /some_path/, @common.theme_path_for('doodley')
+  end
+end
