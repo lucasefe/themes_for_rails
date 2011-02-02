f6a83f9928c9c2735a462cbc51ab1ffae7eab097
Juan Manuel Barreneche <snipperme@gmail.com>
2011-02-02 11:55:03 -0300

    Fix each_theme_dir to use config (alias to available_themes which works correctly)

--- test/themes_for_rails_test.rb
+++ test/themes_for_rails_test.rb
@@ -4,5 +4,11 @@ class ThemesForRailsTest < Test::Unit::TestCase
   should 'know the available themes' do
     assert_equal ['default'], ThemesForRails.available_theme_names
   end
-  
+  should 'use config for each_theme_dir' do
+    ThemesForRails.config.base_dir = Rails.root + 'app'
+    assert_equal ['app_theme'], ThemesForRails.each_theme_dir.map {|f| File.basename(f) }
+  end
+  teardown do
+    ThemesForRails.config.clear
+  end
 end
