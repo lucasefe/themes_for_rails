6c45a8a644f27999afaf344163a947c69c521226
Mark Dickson <dicksonm@med.umich.edu>
2011-03-14 12:28:28 -0400

    added custom theme directory support to routes and rake tasks

--- lib/themes_for_rails/routes.rb
+++ lib/themes_for_rails/routes.rb
@@ -1,9 +1,10 @@
 module ThemesForRails
   module Routes
     def themes_for_rails
-      match 'themes/:theme/stylesheets/*asset' => 'themes_for_rails/assets#stylesheets', :as => :base_theme_stylesheet
-      match 'themes/:theme/javascripts/*asset' => 'themes_for_rails/assets#javascripts', :as => :base_theme_javascript
-      match 'themes/:theme/images/*asset' => 'themes_for_rails/assets#images', :as => :base_theme_image
+      theme_dir = ThemesForRails.config.themes_dir
+      match "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', :as => :base_theme_stylesheet
+      match "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', :as => :base_theme_javascript
+      match "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', :as => :base_theme_image
     end
   end
 end
