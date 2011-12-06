b788818afc27178de6f97ddec37b44f5e28a51be
Lucas Florio <lucasefe@gmail.com>
2011-12-06 17:49:16 -0300

    Sending the right mime_type. Rewriting the whole AssetsController

--- lib/themes_for_rails/assets_controller.rb
+++ lib/themes_for_rails/assets_controller.rb
@@ -2,51 +2,66 @@ require "action_controller/metal"
 
 module ThemesForRails
   class AssetsController < ActionController::Base
-    
+
     include ThemesForRails::CommonMethods
     include ThemesForRails::UrlHelpers
-    
+
     def stylesheets
-      render_asset asset_path_for(params[:asset], 'stylesheets'), mime_type_from(params[:asset])
+      handle_asset(params[:asset], params[:theme], "stylesheets")
     end
-    
+
     def javascripts
-      render_asset asset_path_for(params[:asset], 'javascripts'), mime_type_from(params[:asset])  
+      handle_asset(params[:asset], params[:theme], "javascripts")
     end
-    
+
     def images
-      render_asset asset_path_for(params[:asset], 'images'), mime_type_from(params[:asset])  
+      handle_asset(params[:asset], params[:theme], "images")
     end
-    
+
   private
-  
-    def asset_path_for(asset_url, asset_prefix)
-      File.join(theme_path_for(params[:theme]), asset_prefix, params[:asset])
-    end
     
-    def extract_filename_and_extension_from(asset)
-      extension = File.extname(asset)
-      filename = params[:asset].gsub(extension, '')
-      return filename, extension[1..-1]
-    end
-    
-    def render_asset(asset, mime_type)
-      unless File.exists?(asset)
-        render :text => 'not found', :status => 404
-      else
-        send_file asset, :type => mime_type
+    def handle_asset(asset, theme, prefix)
+      find_asset(asset, theme, prefix) do |path, mime_type|
+        send_file path, :type => mime_type, :disposition => "inline"
       end
     end
-  
-    def mime_type_from(asset_path)
-      extension = extract_filename_and_extension_from(asset_path).last
-      if extension == 'css'
-        "text/css"
-      elsif extension == 'js'
-        'text/javascript'
+    
+    def find_asset(asset_name, asset_theme, asset_prefix)
+      path = asset_path(asset_name, asset_theme, asset_prefix)
+      if File.exists?(path)
+        yield path, mime_type_for(request)
+      elsif File.extname(path).blank?
+        asset_name = "#{asset_name}.#{extension_from(request.path_info)}"
+        return find_asset(asset_name, asset_theme, asset_prefix) 
       else
-        "image/#{extension}"
+        render_not_found
       end
+    end
+
+    def asset_path(asset_name, asset_theme, asset_prefix)
+      File.join(theme_path_for(asset_theme), asset_prefix, asset_name)
+    end
+
+    def render_not_found
+      render :text => 'not found', :status => 404
+    end
+      
+    def mime_type_for(request)
+      existing_mime_type = mime_type_from_uri(request.path_info)
+      unless existing_mime_type.nil? 
+        existing_mime_type.to_s
+      else
+        "image/#{extension_from(request.path_info)}"
+      end
+    end
+    
+    def mime_type_from_uri(path)
+      extension = extension_from(path)
+      Mime::Type.lookup_by_extension(extension)
+    end
+    
+    def extension_from(path)
+      File.extname(path).to_s[1..-1]
     end
   end
 end
