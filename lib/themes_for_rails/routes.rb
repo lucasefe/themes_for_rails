b06859a9f6ecada6aeaf44b14ee31baa595394e7
Lucas Florio <lucasefe@gmail.com>
2012-02-03 09:13:32 -0300

    Better regexp.

--- lib/themes_for_rails/routes.rb
+++ lib/themes_for_rails/routes.rb
@@ -4,18 +4,17 @@ module ThemesForRails
 
     def themes_for_rails
       theme_dir = ThemesForRails.config.themes_dir
-      
+      constraints = { :theme => /[\w\.]*/ } 
+
       match "#{theme_dir}/:theme(/:assets_prefix)/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', 
-        :as => :base_theme_stylesheet, 
-        :constraints => { :theme => /.*/ }
+        :as => :base_theme_stylesheet, :constraints => constraints
 
       match "#{theme_dir}/:theme(/:assets_prefix)/javascripts/*asset" => 'themes_for_rails/assets#javascripts', 
-        :as => :base_theme_javascript, 
-        :constraints => { :theme => /.*/ }
+        :as => :base_theme_javascript, :constraints => constraints
       
       match "#{theme_dir}/:theme(/:assets_prefix)/images/*asset" => 'themes_for_rails/assets#images', 
-        :as => :base_theme_image, 
-        :constraints => { :theme => /.*/ }
+        :as => :base_theme_image, :constraints => constraints
+        
 
     end
 
