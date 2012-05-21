4458f2568c06272e20bc97043bcd8d22765e58f9
roberto <roberto.rabe@idearise.com>
2012-05-20 21:18:18 -0400

    Adding digests to asset helpers.

--- lib/themes_for_rails/url_helpers.rb
+++ lib/themes_for_rails/url_helpers.rb
@@ -3,24 +3,24 @@ module ThemesForRails
   module UrlHelpers
 
     extend ActiveSupport::Concern
-    
+
     included do
-      helper_method :current_theme_stylesheet_path, 
-        :current_theme_javascript_path, 
+      helper_method :current_theme_stylesheet_path,
+        :current_theme_javascript_path,
         :current_theme_image_path
     end
-    
+
     def current_theme_stylesheet_path(asset)
-      base_theme_stylesheet_path(:theme => self.theme_name, :asset => "#{asset}.css")
+      base_theme_stylesheet_path(:theme => self.theme_name, :asset => digest_for("#{asset}.css"))
     end
 
     def current_theme_javascript_path(asset)
-      base_theme_javascript_path(:theme => self.theme_name, :asset => "#{asset}.js")
+      base_theme_javascript_path(:theme => self.theme_name, :asset => digest_for("#{asset}.js"))
     end
 
     def current_theme_image_path(asset)
-      image, extension = asset.split(".")
-      base_theme_image_path(:theme => self.theme_name, :asset => "#{image}.#{extension}")
+      image, extension = name_ext(asset)
+      base_theme_image_path(:theme => self.theme_name, :asset => digest_for("#{image}.#{extension}"))
     end
 
   end
