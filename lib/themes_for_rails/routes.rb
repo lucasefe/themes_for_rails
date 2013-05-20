bcae605289367fe51ff746fca6a93a7f539c6df4
Paul Spieker <p.spieker@duenos.de>
2013-05-20 18:34:05 +0200

    bugfix: route definitions in rails 4

--- lib/themes_for_rails/routes.rb
+++ lib/themes_for_rails/routes.rb
@@ -6,11 +6,11 @@ module ThemesForRails
       theme_dir = ThemesForRails.config.themes_routes_dir
       constraints = { :theme => /[\w\.]*/ } 
       
-      match "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', 
+      get "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', 
         :as => :base_theme_stylesheet, :constraints => constraints
-      match "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', 
+      get "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', 
         :as => :base_theme_javascript, :constraints => constraints
-      match "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', 
+      get "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', 
         :as => :base_theme_image, :constraints => constraints
     end
 
