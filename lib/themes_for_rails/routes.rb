1d40c2ee5996fd5d0510b532cc1d218d2045e7d0
Muhammet DİLEK <dilekmuhammet@gmail.com>
2014-03-09 12:07:21 +0200

    routes fixed for rails4

--- lib/themes_for_rails/routes.rb
+++ lib/themes_for_rails/routes.rb
@@ -6,11 +6,11 @@ module ThemesForRails
       theme_dir = ThemesForRails.config.themes_routes_dir
       constraints = { :theme => /[\w\.]*/ } 
       
-      match "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', 
+      match "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', :via => [:get],
         :as => :base_theme_stylesheet, :constraints => constraints
-      match "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', 
+      match "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', :via => [:get],
         :as => :base_theme_javascript, :constraints => constraints
-      match "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', 
+      match "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', :via => [:get],
         :as => :base_theme_image, :constraints => constraints
     end
 
